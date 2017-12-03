148916ba09605899eeda5f96b5c0da09d1302956
Fredrik Boulund <fredrik.boulund@ki.se>
2017-12-03 21:00:24 +0100

    Unfinished Day 02 subproblem 2. Grappa is hungry.

--- 2017/Day_02_Corruption_checksum/problem_02.d
+++ 2017/Day_02_Corruption_checksum/problem_02.d
@@ -8,18 +8,27 @@ import std.array;
 import std.file : exists;
 
 
-unittest{
-    int[string] example_input_1 = ["5 1 9 5\n7 5 3\n2 4 6 8": 18];
-    foreach (example_string, expected_output; example_input_1)
+auto spreadsheet_rows_checksum(File input_file)
+{
+    int[] rowdiffs;
+    foreach (line; input_file.byLine)
     {
+        auto ints_line = line.splitter.map!(to!int).array;
+        rowdiffs ~= maxElement(ints_line) - minElement(ints_line);
     }
-
+    return sum(rowdiffs);
 }
 
 
-auto spreadsheet_rows_checksum(int[][] input_ints)
+auto even_row_divisions(File input_file)
 {
-    input_ints ~= input_ints[0];
+    int[] rowdivs;
+    foreach (line; input_file.byLine)
+    {
+        auto ints_line = line.splitter.map!(to!int).array;
+        // Todo: divide each value with values in array
+        // until the divison modulo 2 is 0.
+    }
 }
 
 
@@ -28,7 +37,7 @@ int main(string[] argv)
     File input_file;
     if (argv.length < 2)
     {
-        input_file = File("example_sheet.txt");
+        input_file = File("example_input_1");
     } 
     else 
     {
@@ -43,12 +52,7 @@ int main(string[] argv)
         }
     }
 
-    int[] rowdiffs;
-    foreach (line; input_file.byLine)
-    {
-        auto ints_line = line.splitter.map!(to!int).array;
-        rowdiffs ~= maxElement(ints_line) - minElement(ints_line);
-    }
-    writeln(sum(rowdiffs));
+    auto rows_checksum = spreadsheet_rows_checksum(input_file);
+    writeln(rows_checksum);
     return 0;
 }
