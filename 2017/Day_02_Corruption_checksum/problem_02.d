fe12b5803759b09b1aaa995fce477ac7d0f5582b
Fredrik Boulund <fredrik.boulund@ki.se>
2017-12-04 08:56:02 +0100

    Day 02b solved. Had to rewrite the implementation a bit for both problems as I couldn't figure out how to not consume the input File when reading it byLine.

--- 2017/Day_02_Corruption_checksum/problem_02.d
+++ 2017/Day_02_Corruption_checksum/problem_02.d
@@ -3,15 +3,18 @@ import std.conv;
 import std.algorithm : group;
 import std.algorithm.iteration : sum, splitter, map, each, filter;
 import std.algorithm.searching : maxElement, minElement;
+import std.algorithm.comparison : max, min;
+import std.algorithm.mutation : copy;
+import std.math : remainder;
 import std.string : split;
 import std.array;
 import std.file : exists;
 
 
-auto spreadsheet_rows_checksum(File input_file)
+auto spreadsheet_rows_checksum(string[] input_lines)
 {
     int[] rowdiffs;
-    foreach (line; input_file.byLine)
+    foreach (line; input_lines)
     {
         auto ints_line = line.splitter.map!(to!int).array;
         rowdiffs ~= maxElement(ints_line) - minElement(ints_line);
@@ -20,30 +23,42 @@ auto spreadsheet_rows_checksum(File input_file)
 }
 
 
-auto even_row_divisions(File input_file)
+auto even_row_divisions(string[] input_lines)
 {
     int[] rowdivs;
-    foreach (line; input_file.byLine)
+    foreach (line; input_lines)
     {
         auto ints_line = line.splitter.map!(to!int).array;
-        // Todo: divide each value with values in array
-        // until the divison modulo 2 is 0.
+        foreach (i, i1; ints_line)
+        {
+            foreach (i2; ints_line[i+1..$])
+            {
+                auto max_value = to!real(max(i1, i2));
+                auto min_value = to!real(min(i1, i2));
+                if ( remainder(max_value, min_value) == 0)
+                {
+                    rowdivs ~= to!int(max_value/min_value);
+                    break;
+                }
+            }
+        }
     }
+    return sum(rowdivs);
 }
 
 
 int main(string[] argv)
 {
-    File input_file;
+    File[] input_files;
     if (argv.length < 2)
     {
-        input_file = File("example_input_1");
+        input_files = [File("example_input_1")];
     } 
     else 
     {
         if (exists(argv[1]))
         {
-            input_file = File(argv[1]);
+            input_files = [File(argv[1])];
         }
         else
         {
@@ -52,7 +67,14 @@ int main(string[] argv)
         }
     }
 
-    auto rows_checksum = spreadsheet_rows_checksum(input_file);
-    writeln(rows_checksum);
+    foreach (input_file; input_files)
+    {
+        auto input_lines = input_file.byLineCopy.array;
+        auto rows_checksum = spreadsheet_rows_checksum(input_lines);
+        writeln(rows_checksum);
+        auto row_divisions = even_row_divisions(input_lines);
+        writeln(row_divisions);
+    }
+
     return 0;
 }
