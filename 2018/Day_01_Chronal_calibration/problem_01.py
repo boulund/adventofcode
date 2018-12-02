7b761bd245659dabafcf93f6de90321fef356b8c
Fredrik Boulund <fredrik.boulund@ki.se>
2018-12-02 19:18:38 +0100

    Day 01-2 solved.

--- 2018/Day_01_Chronal_calibration/problem_01.py
+++ 2018/Day_01_Chronal_calibration/problem_01.py
@@ -2,7 +2,15 @@
 # AdventOfCode 2018 Day 01
 # Fredrik Boulund 2018
 
-freq_changes = [int(change.strip()) for change in open("input").readlines()]
-print(sum(freq_changes))
+from itertools import cycle, accumulate
 
+freq_changes = [int(change.strip()) for change in open("input").readlines()]
+print(f"Solution 1: {sum(freq_changes)}")
+
+observed_freqs = set()
+for freq in accumulate(cycle(freq_changes)):
+    if freq in observed_freqs:
+        break
+    observed_freqs.add(freq)
+print(f"Solution 2: {freq}")
 
