3e9d12aafdfa0a6aa5ea22ebe58472dad8614dff
Fredrik Boulund <fredrik.boulund@gmail.com>
2021-12-03 16:26:44 +0100

    Completed part Day 3 part one

---
+++ 2021/Day_03_Binary_Diagnostic/03.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+"""AdventOfCode Day 3: Binary Diagnostic"""
+__author__ = "Fredrik Boulund"
+__date__ = "2021-12-03"
+
+from sys import argv, exit
+from collections import Counter, defaultdict
+import argparse
+
+INVERT_BINARY = str.maketrans("01", "10")
+
+def parse_args():
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("problem_input")
+
+    if len(argv) < 2:
+        parser.print_help()
+        exit(1)
+    
+    return parser.parse_args()
+
+def parse_input(input_file):
+    with open(input_file) as f:
+        for line in f:
+            yield line.strip()
+
+def count_bits(problem_input):
+    counters = defaultdict(Counter)
+    for line in problem_input:
+        for pos, value in enumerate(line):
+            counters[pos][value] += 1
+    return counters
+
+def compute_rate(bit_counts):
+    binary_rate = "".join(
+        str(counts.most_common()[0][0]) 
+        for counts in bit_counts.values()
+    )
+    return binary_rate
+
+
+if __name__ == "__main__":
+    args = parse_args()
+
+    problem_input = list(parse_input(args.problem_input))
+
+    bit_counts = count_bits(problem_input)
+
+    binary_gamma_rate = compute_rate(bit_counts)
+    binary_epsilon_rate = compute_rate(bit_counts).translate(INVERT_BINARY)
+    power_consumption = int(binary_gamma_rate, 2) * int(binary_epsilon_rate, 2)
+    print(power_consumption)
+
+    
+
+
+    
+
