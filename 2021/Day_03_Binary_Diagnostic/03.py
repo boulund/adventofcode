1aeab2f05183d017f51ef657db8964bead619cb5
Fredrik Boulund <fredrik.boulund@gmail.com>
2021-12-06 20:55:33 +0100

    Solve Day 03

--- 2021/Day_03_Binary_Diagnostic/03.py
+++ 2021/Day_03_Binary_Diagnostic/03.py
@@ -38,6 +38,27 @@ def compute_rate(bit_counts):
     )
     return binary_rate
 
+def filter_numbers(numbers, co2_rating=True):
+    bitlength = len(numbers[0])
+    for pos in range(bitlength):
+        numbers = filter_on_pos(numbers, pos, co2_rating)
+        if len(numbers) == 1:
+            return numbers.pop()
+
+def filter_on_pos(numbers, pos, co2_rating):
+    new_set = set()
+    bit_counts = count_bits(numbers)[pos]
+    if co2_rating:
+        most_common = bit_counts.most_common()[0][0]
+    else:
+        most_common = bit_counts.most_common()[-1][0]
+    if (bit_counts["0"] == bit_counts["1"]):
+        most_common = "1" if co2_rating else "0"
+    for num in numbers:
+        if num[pos] == most_common:
+            new_set.add(num)
+    return new_set
+
 
 if __name__ == "__main__":
     args = parse_args()
@@ -50,6 +71,11 @@ if __name__ == "__main__":
     binary_epsilon_rate = compute_rate(bit_counts).translate(INVERT_BINARY)
     power_consumption = int(binary_gamma_rate, 2) * int(binary_epsilon_rate, 2)
     print(power_consumption)
+
+    CO2_rating = filter_numbers(problem_input)
+    O2_rating = filter_numbers(problem_input, co2_rating=False)
+    life_support_rating = int(CO2_rating, 2) * int(O2_rating, 2)
+    print(life_support_rating)
 
     
 
