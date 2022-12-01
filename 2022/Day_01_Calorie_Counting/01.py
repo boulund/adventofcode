717fd1377a7aa7105a71b69fd3cd4890a095e3d4
Fredrik Boulund <fredrik.boulund@ki.se>
2022-12-01 08:39:26 +0100

    Day 01, part 2 solved

--- 2022/Day_01_Calorie_Counting/01.py
+++ 2022/Day_01_Calorie_Counting/01.py
@@ -5,8 +5,6 @@ __date__ = "2022-12-01"
 
 from sys import argv, exit
 from itertools import groupby
-from functools import reduce
-
 
 if __name__ == "__main__":
     if len(argv) < 2:
@@ -16,4 +14,5 @@ if __name__ == "__main__":
     elves = [list(int(calories) for calories in group) for k, group in groupby(open(argv[1]).readlines(), key=lambda x: bool(x.strip())) if k]
     elven_sums = [sum(elf) for elf in elves]
     print(max(elven_sums))
+    print(sum(sorted(elven_sums, reverse=True)[:3]))
 
