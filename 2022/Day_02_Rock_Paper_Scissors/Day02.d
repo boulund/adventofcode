9057fc3083bdc6441a09a3201d29c1fb4113afbc
Fredrik Boulund <fredrik.boulund@ki.se>
2022-12-02 08:54:39 +0100

    Completed Day02, part 2

--- 2022/Day_02_Rock_Paper_Scissors/Day02.d
+++ 2022/Day_02_Rock_Paper_Scissors/Day02.d
@@ -42,6 +42,49 @@ int play_rps(char opponent, char me)
     return score;
 }
 
+
+int strategic_rps(char opponent, char me)
+{
+    auto rps_scores = [
+        'X': 1,  //Rock
+        'Y': 2,  //Paper
+        'Z': 3,  //Scissors
+    ];
+    auto rps_strategy = [
+        'X': [  // Lose
+            'A': 'Z',  //op played Rock, choose Scissors
+            'B': 'X',  //op played Paper, choose Rock
+            'C': 'Y',  //op played Scissors, choose Paper
+        ],
+        'Y': [  // Draw
+            'A': 'X',  //op played Rock, choose Rock
+            'B': 'Y',  //op played Paper, choose Paper
+            'C': 'Z',  //op played Scissors, choose Scissors
+        ],
+        'Z': [ // Win
+            'A': 'Y',  //op played Rock, choose Paper
+            'B': 'Z',  //op played Paper, choose Scissors
+            'C': 'X',  //op played Scissors, choose Rock
+        ]
+    ];
+
+    int score;
+    if (me == 'X')
+    {
+        score = rps_scores[rps_strategy[me][opponent]]; 
+    }
+    else if (me == 'Y')
+    {
+        score = rps_scores[rps_strategy[me][opponent]] + 3; 
+    }
+    else if (me == 'Z')
+    {
+        score = rps_scores[rps_strategy[me][opponent]] + 6; 
+    }
+    return score;
+}
+
+
 int main(string[] args)
 {
     if (args.length < 2)
@@ -56,9 +99,16 @@ int main(string[] args)
     {
         char opponent = line[0], me = line[2];
         total_score += play_rps(opponent, me);
-
     }
     writeln(total_score);
+
+    int strategic_score;
+    foreach (line; filename.File.byLine)
+    {
+        char opponent = line[0], me = line[2];
+        strategic_score += strategic_rps(opponent, me);
+    }
+    writeln(strategic_score);
 
     return 0;
 }
