e06d3f6a9550840b3381340a0ef5e8e66438af79
Fredrik Boulund <fredrik.boulund@ki.se>
2022-12-05 22:26:03 +0100

    Day 05, part 1 completed in Python

---
+++ 2022/Day_05_Supply_stacks/05.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""AdventOfCode - Day 05: Supply stacks"""
+__author__ = "Fredrik Boulund"
+__date__ = "2022-12-05"
+
+from sys import argv, exit
+
+if len(argv) < 2:
+    print("usage: 05.py INPUT")
+    exit(1)
+
+def create_stacks(stack_data):
+    stacks = {int(n): [] for n in stack_data[0]}
+    for row in stack_data[1:]:
+        for n, crate in enumerate(row, start=1):
+            if crate != " ":
+                stacks[n].append(crate)
+    return stacks
+
+def parse_input(infile):
+    stack_data = []
+    instructions = []
+    with open(infile) as f:
+        lines = f.readlines()
+        for rownum, line in enumerate(lines):
+            if not line.strip():
+                break
+            stack_data.append(line[1:-1:4])
+        for line in lines[rownum+1:]:
+            _, num, _, src, _, dest = line.split()
+            instructions.append((int(num), int(src), int(dest)))
+    return create_stacks(stack_data[::-1]), instructions
+
+def execute(stacks, instructions):
+    for num, src, dest in instructions:
+        for n in range(num):
+            stacks[dest].append(stacks[src].pop())
+    return stacks
+
+
+stacks, instructions = parse_input(argv[1])
+
+result = execute(stacks, instructions)
+print("".join([l[-1] for s, l in result.items()]))
