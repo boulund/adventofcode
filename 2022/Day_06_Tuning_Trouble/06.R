8b94fef3641907d97c32235f3d9d07bd056707a9
Fredrik Boulund <fredrik.boulund@ki.se>
2022-12-06 08:46:50 +0100

    Day 6, part 2 solved. Removed dependency on readr package

--- 2022/Day_06_Tuning_Trouble/06.R
+++ 2022/Day_06_Tuning_Trouble/06.R
@@ -1,18 +1,23 @@
 #!/usr/bin/env Rscript
 
-require(readr)
-
 args <- commandArgs(trailingOnly = T)
 if (length(args) != 1) {
 	cat("usage: 06.R INPUT")
 	quit()
 }
 
-data <- read_lines(args[1])
+data <- read.table(args[1])[[1]][1]
 
+found.marker <- F
 for (i in 4:(nchar(data)-4)) {
-	if (length(unique(strsplit(substr(data, i, i+3),"")[[1]])) == 4) {
-		cat("First marker at: ", i+3)
+	if (!found.marker & length(unique(strsplit(substr(data, i, i+3),"")[[1]])) == 4) {
+		cat("First marker at: ", i+3, "\n")
+		found.marker <- T
+	}
+	if (length(unique(strsplit(substr(data, i, i+13),"")[[1]])) == 14) {
+		cat("Message marker at: ", i+13, "\n")
 		break
 	}
+
 }
+
