d7ab4e535449cfdf2bd6c7d8648464a50e853e5d
Fredrik Boulund <fredrik.boulund@ki.se>
2022-12-07 21:38:52 +0100

    Day 7, part 1 in Python. Not very elegant

---
+++ 2022/Day_07_No_space_left_on_device/07.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+"""AdventOfCode 2022 - Day 07: No space left on device"""
+__author__ = "Fredrik Boulund"
+__date__ = "2022-12-07"
+
+from sys import argv, exit
+
+if len(argv) < 2:
+    print("usage: 07.py INPUT")
+    exit(1)
+
+root = {"name": "/", "type": "d", "size": 0, "children": [], "parent": {}}
+
+in_listing = False
+with open(argv[1]) as f:
+    cwd = root
+    for line in f.readlines()[1:]:
+        if line.startswith("$ cd"):
+            _, _, name = line.strip().split()
+            if name == "..":
+                cwd = cwd["parent"]
+            elif name == "/":
+                cwd = root
+            else:
+                new_dir = {"name": name, "type": "d", "size": 0, "children": [], "parent": cwd}
+                cwd["children"].append(new_dir)
+                cwd = new_dir
+        elif line.startswith("$ ls"):
+            in_listing = True
+        elif in_listing:
+            ts, name = line.strip().split()
+            if ts != "dir":
+                new_file = {"name": name, "type": "f", "size": int(ts), "children": None, "parent": cwd}
+                cwd["children"].append(new_file)
+                
+
+def calculate_node_totals(node, l=0):
+    #print(f"{'  '*l}{node['name']} {node['type']} {node['size']}")
+    if node["children"]:
+        for c in node["children"]:
+            size = calculate_node_totals(c, l=l+1)
+            node["size"] += size
+    return node["size"]
+
+calculate_node_totals(root)
+#print(root["size"])
+
+def get_dir_sizes(node):
+    node_sizes = {}
+    if node["children"]:
+        for c in node["children"]:
+            node_sizes = {**node_sizes, **get_dir_sizes(c)}
+    if node["type"] == "d":
+        node_sizes[f"{node['parent'].get('name')}/{node['name']}"] = node["size"]
+    return node_sizes
+
+#print(get_dir_sizes(root))
+
+total = 0
+for dirname, size in get_dir_sizes(root).items():
+    if size <= 100000:
+        total += size
+        #print(dirname, size)
+
+print(total)
