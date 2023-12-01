706a03a3f717d0ed13b47baa1320f6740ff4ea4d
Fredrik Boulund <fredrik.boulund@gmail.com>
2023-12-01 07:51:55 +0100

    Day 01 solved

--- 2023/Day_01_Trebuchet/01.py
+++ 2023/Day_01_Trebuchet/01.py
@@ -9,9 +9,29 @@ if len(argv) < 2:
     print("usage: 01.py input")
     exit(1)
 
+nums = {
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9,
+    "zero": 0,
+}
+
 all_digits = []
 for line in open(argv[1]).readlines():
-    all_digits.append([d for d in line if d.isdigit()])
+    digits = []
+    for idx, _ in enumerate(line):
+        for num, number in nums.items():
+            if line[idx:].startswith(num):
+                digits.append(number)
+        if line[idx].isdigit():
+            digits.append(int(line[idx]))
+    all_digits.append(digits)
 
 print(sum(int(f"{digit[0]}{digit[-1]}") for digit in all_digits))
 
