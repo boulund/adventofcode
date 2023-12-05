dc165a14006e8138d7899254c06aacb9e1f17bb8
Fredrik Boulund <fredrik.boulund@ki.se>
2023-12-05 14:02:17 +0100

    Day 04 solved

--- 2023/Day_04_Scratchcards/04.py
+++ 2023/Day_04_Scratchcards/04.py
@@ -5,27 +5,38 @@ __date__ = "2023"
 
 from sys import argv, exit
 from math import floor
+from collections import defaultdict
 
 if len(argv) < 2:
     print("usage: 04.py INPUT")
     exit(1)
 
-cards = {}
+cards = []
 
 with open(argv[1]) as f:
     for line in f:
         card_n, data = line.split(":")
-        card = card_n.split()[1]
         draw, winning = data.split("|")
         draw = set(draw.split())
         winning = set(winning.split())
-        cards[card] = (draw, winning)
+        cards.append((draw, winning))
 
-def score(draw, winning):
+def score_draw(draw, winning):
     intersection = draw.intersection(winning)
-    return floor(2**(len(intersection)-1))
+    return floor(2**(len(intersection)-1)), len(intersection)
 
 scores = []
-for card, data in cards.items():
-    scores.append(score(*data))
+winnings = []
+total_cards = defaultdict(int)
+for idx, numbers in enumerate(cards, start=1):
+    score, num = score_draw(*numbers)
+    scores.append(score)
+    winnings.append(num)
+    total_cards[idx] += 1
 print(sum(scores))
+
+for idx, num in enumerate(winnings, start=1):
+    for card in range(idx+1, idx+1+num):
+        total_cards[card] += total_cards[idx]
+print(sum(total_cards.values()))
+
