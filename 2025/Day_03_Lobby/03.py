7f9d59796502472ab88b0c89b26b3464b48f8158
Fredrik Boulund <fredrik.boulund@ki.se>
2025-12-04 10:07:32 +0100

    Day 03 solved

--- 2025/Day_03_Lobby/03.py
+++ 2025/Day_03_Lobby/03.py
@@ -4,7 +4,7 @@ __date__ = "2025-12-03"
 """ Advent of Code 2025, day 03"""
 
 from sys import argv, exit
-import re
+from functools import reduce
 
 if len(argv) < 2:
     print("usage: 03.py INPUT")
@@ -22,3 +22,20 @@ for bank in banks:
     max_joltages.append(max_joltage)
 
 print(sum(max_joltages))
+
+all_maxes = []
+for bank in banks:
+    max_jolts = []
+    joltages = [int(joltage) for joltage in bank]
+    prev_max = 0
+    for pos in range(12):
+        range_end = len(joltages)-(11 - len(max_jolts))
+        subrange = joltages[prev_max:range_end]
+        max_ = subrange.index(max(subrange))
+        max_jolts.append(subrange[max_])
+        prev_max = prev_max + max_ + 1
+    max_joltage = int(reduce(lambda x, y: str(x)+str(y), max_jolts))
+    all_maxes.append(max_joltage)
+
+print(sum(all_maxes))
+
