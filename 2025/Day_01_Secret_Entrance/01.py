b995c5112f4742166d8a52867735bae844093c92
Fredrik Boulund <fredrik.boulund@ki.se>
2025-12-04 15:28:34 +0100

    Day 01 completed

--- 2025/Day_01_Secret_Entrance/01.py
+++ 2025/Day_01_Secret_Entrance/01.py
@@ -14,14 +14,26 @@ with open(argv[1]) as infile:
 
 dial = list(range(0,100))
 current_pos = 50
+exactly_zero = 0
 zero_count = 0
-for rot in sequence:
-    direction, value = rot[0], int(rot[1:])
+for rotation in sequence:
+    direction, value = rotation[0], int(rotation[1:])
+    full_rotations = value // 100
+    zero_count += full_rotations
+    value = value % 100  # Remainder after full rotations
     if direction == "L":
-        current_pos = dial[(current_pos - value) % 100]
+        new_pos = current_pos - value
+        if current_pos != 0 and new_pos <= 0: 
+            zero_count += 1
+        current_pos = dial[new_pos % 100]
     else:
-        current_pos = dial[(current_pos + value) % 100]
+        new_pos = current_pos + value
+        current_pos = dial[new_pos % 100]
+        if new_pos >= 100:
+            zero_count += 1
     if current_pos == 0:
-        zero_count += 1
-    print(current_pos)
-print("Zero count:", zero_count)
+        exactly_zero += 1
+    #print(rotation, current_pos, zero_count)
+
+print("Exactly zero count:", exactly_zero)
+print("Passing zero count:", zero_count)
