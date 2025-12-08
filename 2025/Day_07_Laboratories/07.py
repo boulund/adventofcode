5da26c9b41496eaf137d6d2b666df938b1630d8f
Fredrik Boulund <fredrik.boulund@ki.se>
2025-12-08 02:52:33 +0100

    Day 07 part 2 solved

--- 2025/Day_07_Laboratories/07.py
+++ 2025/Day_07_Laboratories/07.py
@@ -12,15 +12,21 @@ if len(argv) < 2:
 rows = [r.strip() for r in open(argv[1]).readlines()]
 
 beams = [False for b in rows[0]]
+timelines = [0 for b in rows[0]]
 splitcount = 0
 for row in rows:
     for idx, col in enumerate(row):
         if col == "S":
             beams[idx] = True
+            timelines[idx] = 1
         elif col == "^" and beams[idx]:
             beams[idx] = False
             beams[max(idx-1, 0)] = True
             beams[min(idx+1, len(beams))] = True
             splitcount += 1
+            timelines[idx-1] += timelines[idx]
+            timelines[idx+1] += timelines[idx]
+            timelines[idx] = 0
 print(splitcount)
+print(sum(timelines))
 
